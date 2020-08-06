a4109cb4e47a0e6f8b289dedd0e0e37adbca22d0
Jacob Sada <spurius1987@gmail.com>
2020-08-06 15:27:29 -0500

    Updated html tab name and removed select.js

--- src/components/Select.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Select = ({ data }) => {
-  const selection = data.map((el, i) => <option key={i}>{el.state}</option>);
-  return <select>{selection}</select>;
-};
-
-export default Select;
